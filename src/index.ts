59ecb067ed3245ee986043dda39dbbb61b3e0ede
Sorunome <mail@sorunome.de>
2021-02-05 16:04:42 +0100

    update copyright headers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2020 Famedly
+Copyright (C) 2020, 2021 Famedly
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
